eb3a2735eeffe1faadf7ab64b732bcf354b92878
t-takaai <takoyaj@gmail.com>
2016-06-09 15:01:14 +0900

    part2_lesson24_error

--- src/components/video_list.js
+++ src/components/video_list.js
@@ -1,10 +1,24 @@
 import React from 'react';
 import VideoListItem from './video_list_item';
 
+// const VideoList = (props) => {
+//   return (
+//     <ul className="col-md-4 list-group">
+//       {props.videos.length}
+//     </ul>
+//   );
+// };
+//
+// export default VideoList;
+
 const VideoList = (props) => {
+  const videoItems = props.videos.map((video) => {
+    return <VideoListItem video={video} />;
+  });
+
   return (
     <ul className="col-md-4 list-group">
-      {props.videos.length}
+      {videoItems}
     </ul>
   );
 };
